39888e0a9c4866057842909c2231fd8db1cfef6c
yaiza <yaizadelrosarioguedessantana@alumno.ieselrincon.es>
2026-02-24 17:52:00 +0000

    Final changes to generate .jar

--- autolog/src/main/java/com/autolog/autolgapi/AutologApplication.java
+++ autolog/src/main/java/com/autolog/autolgapi/AutologApplication.java
@@ -1,19 +1,22 @@
 package com.autolog.autolgapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
 @EnableJpaRepositories(basePackages = "com.autolog.repository")
 @EntityScan(basePackages = "com.autolog.model")
 @ComponentScan(basePackages="com.autolog")
 public class AutologApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutologApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(AutologApplication.class);
+        app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.run(args);
+    }
 }
